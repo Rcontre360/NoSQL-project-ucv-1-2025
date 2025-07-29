b04be191acceb78cb7ed497d7c702f7d39702c5f
Rcontre360 <rcontreraspimentel@gmail.com>
2025-07-28 23:34:53 -0400

    feat: added few elements and setup etl

--- etl/script.py
+++ etl/script.py
@@ -1,70 +1,173 @@
+from typing import List
 from pymongo import MongoClient
-from neo4j import GraphDatabase
+from pymongo.synchronous.database import Database
+from neo4j import GraphDatabase, Session
+
+from etl_types import CollectionContext, Document, MigrateCollection
 
 # Configuración de Neo4j
 neo4j_uri = "neo4j://127.0.0.1:7687"
 neo4j_user = "neo4j"
-neo4j_password = "bdyari123"
+neo4j_password = "12345678"
 
 # Configuración de MongoDB
 mongo_uri = "mongodb://localhost:27017"
-mongo_db_name = "NoSQLProy"
-mongo_collection_name = "vehicle"
+mongo_db_name = "nosql"
+
+def get_connections():
+    try:
+        mongo_client = MongoClient(mongo_uri)
+        driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+        driver.verify_connectivity()
+
+        return mongo_client[mongo_db_name],driver
+    except Exception as e:
+        print(f"Error during connection: {e}")
+        raise # stops the program
+
+def clean_node_res(res:list[Document]) -> list[Document]:
+    return list(map(lambda x: {**x['n'], '_id': x['node_id']}, res))
+
+def migrate_species(db: Database, session: Session, context: CollectionContext) -> List[Document]:
+    collection_name = "specie"
+    collection = db[collection_name]
+    documents = list(collection.find())
+
+    for doc in documents:
+        if '_id' in doc:
+           doc.pop('_id')
+
+    # we insert all in a single operation
+    cypher_query = """
+       UNWIND $documents AS doc
+       CREATE (n:Specie)
+       SET n = doc
+       RETURN n, elementId(n) as node_id
+    """
+
+    res = clean_node_res(session.run(cypher_query, documents=documents).data())
+    context.species = res
+    return res
+
+def migrate_weapons(db:Database, session:Session, context: CollectionContext) -> List[Document]:
+    collection_name = "character"
+    collection = db[collection_name]
+    documents = list(collection.find())
+    weapons = []
+
+    for doc in documents:
+        weapons.append(doc['weapon'])
+
+    cypher_query = """
+       UNWIND $documents AS doc
+       CREATE (n:Weapon)
+       SET n = doc
+       RETURN n, elementId(n) as node_id
+    """
+
+    result = clean_node_res(session.run(cypher_query, documents=weapons).data())
+    context.weapons = result
+    return result
+
+def migrate_vehicles(db: Database, session: Session, context: CollectionContext) -> List[Document]:
+    collection_name = "vehicle"
+    collection = db[collection_name]
+    documents = list(collection.find())
+
+    for doc in documents:
+        if '_id' in doc:
+           doc.pop('_id')
+
+    # we insert all in a single operation
+    cypher_query = """
+       UNWIND $documents AS doc
+       CREATE (n:Vehicle)
+       SET n = doc
+       RETURN n, elementId(n) as node_id
+    """
+
+    res = clean_node_res(session.run(cypher_query, documents=documents).data())
+    context.vehicles = res
+    return res
+
+def migrate_faction(db: Database, session: Session, context: CollectionContext) -> List[Document]:
+    collection_name = "faction"
+    collection = db[collection_name]
+    documents = list(collection.find())
+
+    for doc in documents:
+        if '_id' in doc:
+           doc.pop('_id')
+
+    # we insert all in a single operation
+    cypher_query = """
+       UNWIND $documents AS doc
+       CREATE (n:Faction)
+       SET n = doc
+       RETURN n, elementId(n) as node_id
+    """
+
+    res = clean_node_res(session.run(cypher_query, documents=documents).data())
+    context.factions = res
+    return res
+
+def migrate_planets(db: Database, session: Session, context: CollectionContext) -> List[Document]:
+    collection_name = "planet"
+    collection = db[collection_name]
+    documents = list(collection.find())
+
+    for doc in documents:
+        if '_id' in doc:
+           doc.pop('_id')
+        for specie in doc['species_ids']:
+            pass
+
+    cypher_query = """
+       UNWIND $documents AS doc
+       CREATE (n:Planet)
+       SET n = doc
+       RETURN n, elementId(n) as node_id
+    """
+
+    res = clean_node_res(session.run(cypher_query, documents=documents).data())
+    context.planets = res
+    return res
+
+def migrate_characters(db:Database):
+    pass
+
+def migrate_spaceships(db:Database):
+    pass
+
+def migrate_locations(db:Database):
+    pass
+
+def migrate_movies(db:Database):
+    pass
+
+def migrate_historic_events(db:Database):
+    pass
+
+migrators:List[MigrateCollection] = [migrate_species, migrate_weapons, migrate_vehicles, migrate_faction]
 
 def migrate_data():
+    db,target = get_connections()
+    context = CollectionContext()
 
-    try:
-        # Conexión a MongoDB
-        mongo_client = MongoClient(mongo_uri)
-        print("mongo_client: ", mongo_client)
-        db = mongo_client[mongo_db_name]
-        print("db: ", db)
-        collection = db[mongo_collection_name]
-        print("collection: ", collection)
+    with target.session() as session:
+        # only during development
+        clear_neo4j(session)
 
-        # Conexión a Neo4j
-        driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+        for migrator in migrators:
+            migrator(db,session,context)
 
-        # Obtención de colecciones MongoDB
-        collection_names = db.list_collection_names()
-        print(f"Colecciones encontradas en '{mongo_db_name}': {collection_names}")
-
-        with driver.session() as session:
-            for collection_name in collection_names:
-                print(f"\n--- Migrando colección: '{collection_name}' ---")
-                collection = db[collection_name]
-
-                for document in collection.find():
-                    print(document)
-                    session.execute_write(create_node_from_document, document, collection_name)
-                print(f"Colección '{collection_name}' migrada exitosamente.")
-
-    except Exception as e:
-        print(f"Error durante la migración: {e}")
-    finally:
-        if 'mongo_client' in locals():
-            mongo_client.close()
-        if 'driver' in locals():
-            driver.close()
-
-
-def create_node_from_document(tx, document, collection_label):
-    if '_id' in document:
-        document.pop('_id')
-
-    properties_parts = []
-    for key, value in document.items():
-        if isinstance(value, str):
-            escaped_value = value.replace("'", "\\'")
-            properties_parts.append(f"{key}: '{escaped_value}'")
-        else:
-            properties_parts.append(f"{key}: {value}")
-
-    properties_string = ", ".join(properties_parts)
-    
-    query = f"CREATE (n:{collection_label} {{{properties_string}}})"
-    tx.run(query)
-
+def clear_neo4j(session: Session) -> None:
+   cypher_query = """
+   MATCH (n)
+   DETACH DELETE n
+   """
+   session.run(cypher_query)
+   print("Neo4j cleared successfully")
 
 if __name__ == "__main__":
     migrate_data()
